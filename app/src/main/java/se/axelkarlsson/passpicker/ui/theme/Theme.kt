255b70cb2cf27d27a5d38a494bf6403f21b4dc5c
Axel H. Karlsson <git@axelkarlsson.se>
2026-06-04 16:14:32 +0200

    resolve #6

--- app/src/main/java/se/axelkarlsson/passpicker/ui/theme/Theme.kt
+++ app/src/main/java/se/axelkarlsson/passpicker/ui/theme/Theme.kt
@@ -2,9 +2,10 @@ package se.axelkarlsson.passpicker.ui.theme
 
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.darkColorScheme
-import androidx.compose.material3.lightColorScheme
+import androidx.compose.material3.dynamicDarkColorScheme
+import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.platform.LocalContext
 
 @Composable
 fun PassPickerTheme(
@@ -12,9 +13,9 @@ fun PassPickerTheme(
     content: @Composable () -> Unit
 ) {
     val colorScheme = if (darkTheme) {
-        darkColorScheme()
+        dynamicDarkColorScheme(LocalContext.current)
     } else {
-        lightColorScheme()
+        dynamicLightColorScheme(LocalContext.current)
     }
 
     MaterialTheme(
