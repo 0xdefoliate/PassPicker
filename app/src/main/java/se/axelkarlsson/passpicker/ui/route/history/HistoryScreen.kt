6a44f4f8bfdfa7613abe30ff77fe21e23a7472ed
Axel H. Karlsson <git@axelkarlsson.se>
2026-06-07 09:32:55 +0200

    show history tab as not implemented

--- app/src/main/java/se/axelkarlsson/passpicker/ui/route/history/HistoryScreen.kt
+++ app/src/main/java/se/axelkarlsson/passpicker/ui/route/history/HistoryScreen.kt
@@ -1,8 +1,18 @@
 package se.axelkarlsson.passpicker.ui.route.history
 
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 
 @Composable
 fun HistoryScreen() {
-
+    Box(
+        modifier = Modifier.fillMaxSize(),
+        contentAlignment = Alignment.Center
+    ) {
+        Text("History isn't yet implemented")
+    }
 }
