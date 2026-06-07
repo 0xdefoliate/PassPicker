4175ae4af7a72af34ce4015da442aa0a15d2a025
Axel H. Karlsson <git@axelkarlsson.se>
2026-06-07 09:58:51 +0200

    bump => v1.0.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -38,8 +38,8 @@ android {
         applicationId = "se.axelkarlsson.passpicker"
         minSdk = 33
         targetSdk = 36
-        versionCode = 1
-        versionName = "0.1.0"
+        versionCode = 2
+        versionName = "1.0.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
